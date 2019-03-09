3f51331d49bfcdbe4e59c13c4b732ac712e4d90c
imaldon4 <imaldon4@mail.ccsf.edu>
2019-03-08 22:38:43 -0800

    Initial commit

---
+++ app/src/main/java/com/example/renaissancepaintings/RenaissanceDatabase.java
@@ -0,0 +1,19 @@
+package com.example.renaissancepaintings;
+
+public class RenaissanceDatabase {
+
+    public static String description[] = {
+            "Venus of Urbino\nTitan, 1538",
+            "St.John the Baptist\nLeonardo da Vinci, 1516",
+            "A boy and faggot\nMike Monroe, 1231",
+            "This is losing everything\nWe have nothing, 1233"
+    };
+
+    public static int id[] = {
+            R.drawable.dmcdonald,
+            R.drawable.esualdochapel,
+            R.drawable.office,
+            R.drawable.preren,
+
+    };
+}
